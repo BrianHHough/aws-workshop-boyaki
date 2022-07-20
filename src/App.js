354c0c8f01e4da3d358a7773c5be3bfadf873cd8
Brian H. Hough | brianhuff.eth <56947075+BrianHHough@users.noreply.github.com>
2022-07-20 02:53:06 -0400

    Added BrowserRouter + Routes

--- src/App.js
+++ src/App.js
@@ -7,27 +7,72 @@ import { useAuthenticator } from "@aws-amplify/ui-react";
 
 import {Login} from "./components/Login"
 
-import {
-  HashRouter,
-  Switch,
+import { 
+  Navigate,
+  Routes, 
   Route,
-  Redirect,
-} from 'react-router-dom';
+  BrowserRouter } from 'react-router-dom';
 
-import { makeStyles, ThemeProvider } from "@mui/material/styles"
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles"
+import CssBaseline from '@mui/material/CssBaseline';
+import { red } from '@mui/material/colors';
+// import Checkbox from '@mui/material/Checkbox';
 
 import awsconfig from './aws-exports';
 import HomePage from './components/Homepage';
 Amplify.configure(awsconfig);
 
-function App({ signOut, userState }) {
-  const [authState, setAuthState] = useState();
+const theme = createTheme({
+  status: {
+    danger: red[500],
+  },
+  palette: {
+    // type: 'light',
+      // primary: {
+      //   main: '#f50057',
+      // },
+      // secondary: {
+      //   main: '#f50057',
+      // },
+      background: {
+        default: 'rgb(15, 12, 41)',
+      },
+      text: {
+        primary: 'white',
+      },
+    },
+});
+
+// const CustomCheckbox = styled(Checkbox)(({ theme }) => ({
+//   color: theme.status.danger,
+//   '&.Mui-checked': {
+//     color: theme.status.danger,
+//   },
+// }));
+
+function App() {
   const { user } = useAuthenticator();
-  const { tokens } = useTheme();
 
       if (user) {
-        return <HomePage/>
-      }
+        return (
+          <>
+            <ThemeProvider theme={theme}>
+              <CssBaseline /> {/* Remove styles */}
+              <BrowserRouter>
+                <Routes> {/* No longer Switch */}
+                  <Route path='/' element={<HomePage/>} />
+                  <Route path='/global-timeline' element={<HomePage/>} />
+                  <Route path='/:userId' element={<HomePage/>} />
+                  <Route path="/*" element={<Navigate to="/" />} />
+                  {/* <Navigate path='*' to="/" /> */}
+                </Routes> 
+                {/* <Navigate path='*' to="/" /> */}
+              </BrowserRouter>
+            </ThemeProvider>
+          </>
+        )
+      };
+
       return <Login/>
       }
 
--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -14,7 +14,7 @@ export function HomePage(){
         <div>Hey you're logged in</div>
         <Authenticator>
             {({ signOut, user }) => (
-              <main style={{color: "black"}}>
+              <main>
                 <h1>Hello, {user.username}</h1>
                 <button onClick={signOut}>Sign out</button>
               </main>
